61d5ad1480ade061cffd38ddb5b6b33a1725ae6e
MaithilyKawale <maithilyworkspace@gmail.com>
2026-01-08 01:17:21 +0530

    card number added

--- src/views/Payment.jsx
+++ src/views/Payment.jsx
@@ -53,3 +53,18 @@ const Payment = () => {
             />
           </div>
 
+          {/* Card Number */}
+          <div className="mb-4">
+            <label className="block mb-2">Card Number</label>
+            <input
+              type="text"
+              name="cardNumber"
+              placeholder="1234 5678 9012 3456"
+              value={paymentData.cardNumber}
+              onChange={handleChange}
+              required
+              maxLength="16"
+              className="w-full px-4 py-2 border rounded-md"
+            />
+          </div>
+
