071b3cc51ba6ee5106aad4467d4470cf1db4dc0f
MaithilyKawale <maithilyworkspace@gmail.com>
2026-01-08 01:02:57 +0530

    button added

--- src/views/Register.jsx
+++ src/views/Register.jsx
@@ -90,3 +90,26 @@ const Register = () => {
               className="w-full px-4 py-2 border rounded-md"
             />
           </div>
+
+          {/* Register Button */}
+          <button
+            type="submit"
+            className="w-full bg-green-600 text-white py-2 rounded-md hover:bg-green-700"
+          >
+            Register
+          </button>
+        </form>
+
+        <p className="text-center mt-4">
+          Already have an account?{" "}
+          <Link to="/login" className="text-blue-600">
+            Login
+          </Link>
+        </p>
+
+      </div>
+    </div>
+  );
+};
+
+export default Register;
