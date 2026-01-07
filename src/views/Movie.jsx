0201d825c61d11297babfa0f647232599aef0d6c
siddhikharde <siddhikharde2005@gmail.com>
2026-01-07 16:20:17 +0530

    search input created

--- src/views/Movie.jsx
+++ src/views/Movie.jsx
@@ -2,7 +2,17 @@ import React from 'react'
 
 function Movie() {
   return (
-    <div>Movie</div>
+   <div className='bg-[#1B3C53] min-h-screen'>
+            <div className='flex flex-col w-[100%]  md:flex-row gap-5 items-center justify-center  p-10 '>
+                <div className='md:w-[50%] w-[100%]  flex items-center justify-center'>
+                    <input type='text' placeholder='Search Movies by title ....'
+                        onChange={(e) => {
+                            setSearchMovies(e.target.value)
+                        }} className='border px-5 w-[100%] py-2 hover:scale-3d text-gray-300 focus:outline-none rounded-[4px] border-[1.5px]  border-[#456882]' />
+                </div>
+                </div>
+                </div>
+
   )
 }
 
