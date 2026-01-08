adea9b5ceafba10d857925fae356dc0d4271d5d5
siddhikharde <siddhikharde2005@gmail.com>
2026-01-08 23:22:07 +0530

    hero section created

--- src/views/About.jsx
+++ src/views/About.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 function About() {
   return (
-    <div>About</div>
+    <div className='bg-[#1B3C53] min-h-screen p-1'>
+      <div className='flex  items-center bg-[#234C6A] mt-18  flex-col gap-7 md:px-10 py-15 justify-center p-5 text-2xl text-white text-center' >
+        <h1 className='text-[40px] md:text-[55px] font-bold text-[#E3E3E3]'>About MoveZone</h1>
+        <p className='md:text-xl text-lg  text-gray-400  md:w-[60%] w-[90%]'>Your ultimate destination for seamless movie ticket booking. We bring the magic of cinema right to your fingertips.</p>
+      </div>
+    </div>
   )
 }
 
