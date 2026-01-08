4f7d812d01571e4d76de9662a8557c6cc650349d
siddhikharde <siddhikharde2005@gmail.com>
2026-01-08 23:32:10 +0530

    config created for features cards

--- src/views/About.jsx
+++ src/views/About.jsx
@@ -1,4 +1,26 @@
 import React from 'react'
+import { Film,Users, ShieldCheck, MapPin } from 'lucide-react'
+
+const fatures=[
+  {
+    image:<Film />,
+    title:"500+",
+    description:"Movies Available"
+  },
+  {
+    image:<Users />,
+    title:"1M+",
+    description:"Happy Customers"
+  },{
+    image:<ShieldCheck />,
+    title:"50+",
+    description:"Partner Cinemas"
+  },{
+    image:<MapPin />,
+    title:"100+",
+    description:"Cities Covered"
+  }
+]
 
 function About() {
   return (
