34a1a3d864f328779d5fb0716329d5d169b4ef99
siddhikharde <siddhikharde2005@gmail.com>
2026-01-09 00:29:37 +0530

    our mission section added

--- src/views/About.jsx
+++ src/views/About.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Film,Users, ShieldCheck, MapPin } from 'lucide-react'
 import Button from '../components/Button';
 import { useNavigate } from 'react-router';
+import MissonImage from "../assets/about-img/mission.jpg"
 
 const features=[
   {
@@ -27,12 +28,12 @@ const features=[
 function About() {
   const navigate=useNavigate();
   return (
-    <div className='bg-[#1B3C53] min-h-screen p-1'>
+    <div className='bg-[#1B3C53] min-h-screen p-5 box-border'>
       <div className='flex  items-center bg-[#234C6A] mt-18  flex-col gap-7 md:px-10 py-15 justify-center p-5 text-2xl text-white text-center' >
         <h1 className='text-[40px] md:text-[55px] font-bold text-[#E3E3E3]'>About MoveZone</h1>
         <p className='md:text-xl text-lg  text-gray-400  md:w-[60%] w-[90%]'>Your ultimate destination for seamless movie ticket booking. We bring the magic of cinema right to your fingertips.</p>
       </div>
-      <div className='flex flex-wrap md:flex-nowrap  w-[100%] mt-10 gap-5 p-3 '>
+      <div className='flex flex-wrap items-center justify-center md:flex-nowrap  w-[100%] mt-10 gap-5 p-3 '>
         {
           features.map((item, index)=>{
             const{image, title, description}=item;
@@ -47,16 +48,16 @@ function About() {
         }
       </div>
 
-      <div className='flex flex-col md:flex-row my-10 mx-5 bg-[#234C6A] w-full items-center justify-center p-5 gap-5'>
-        <div className='flex flex-col gap-5 items-start px-7 text-white justify-start'>
-          <h1 className='text-2xl font-bold'> Our Mission</h1>
-          <p>At MoveZone, we believe that everyone deserves easy access to the magic of movies. Our mission is to revolutionize the way you experience cinema by providing a seamless, intuitive, and delightful ticket booking experience.</p>
-          <p>Whether you're planning a date night, a family outing, or a solo adventure into the world of film, MoveZone is here to make your experience unforgettable from the moment you book to the final credits.</p>
-          <Button title={"Start Booking "} variant='primary' size='md' onClick={()=>navigate("/movie")}/>
+      <div className='flex flex-col md:flex-row my-10 rounded-2xl bg-[#234C6A] w-full items-center justify-center p-5 gap-5'>
+        <div className='flex flex-col gap-5 items-start md:pl-5 text-white justify-start w-full md:w-1/2 '>
+          <h1 className='text-3xl font-bold'> Our Mission</h1>
+          <p className='text-[17px] text-gray-400'> At MoveZone, we believe that everyone deserves easy access to the magic of movies. Our mission is to revolutionize the way you experience cinema by providing a seamless, intuitive, and delightful ticket booking experience.</p>
+          <p className='text-[17px] text-gray-400'>Whether you're planning a date night, a family outing, or a solo adventure into the world of film, MoveZone is here to make your experience unforgettable from the moment you book to the final credits.</p>
+          <Button title={"Start Booking "} variant='primary' size='lg' onClick={()=>navigate("/movie")}/>
         </div>
-        <div>
-          <img src="" alt="" />
-        </div>
+        <div className='md:w-1/2 w-full py-5 flex items-center justify-center'>
+    <img src={MissonImage} alt="Mission Image" className='rounded-2xl w-full max-w-[500px] object-cover' />
+  </div>
       </div>
     </div>
   )
