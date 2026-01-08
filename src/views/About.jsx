3190794fedf8b6b8e60e91271241c51114862ebd
siddhikharde <siddhikharde2005@gmail.com>
2026-01-09 00:00:21 +0530

    image added

--- src/views/About.jsx
+++ src/views/About.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import { Film,Users, ShieldCheck, MapPin } from 'lucide-react'
+import Button from '../components/Button';
+import { useNavigate } from 'react-router';
 
 const features=[
   {
@@ -23,6 +25,7 @@ const features=[
 ]
 
 function About() {
+  const navigate=useNavigate();
   return (
     <div className='bg-[#1B3C53] min-h-screen p-1'>
       <div className='flex  items-center bg-[#234C6A] mt-18  flex-col gap-7 md:px-10 py-15 justify-center p-5 text-2xl text-white text-center' >
@@ -39,10 +42,21 @@ function About() {
               <h1 className='text-[#E3E3E3] text-3xl font-bold'>{title}</h1>
               <p className='text-gray-400 '>{description}</p>
             </div>
-
             )
           })
         }
+      </div>
+
+      <div className='flex flex-col md:flex-row my-10 mx-5 bg-[#234C6A] w-full items-center justify-center p-5 gap-5'>
+        <div className='flex flex-col gap-5 items-start px-7 text-white justify-start'>
+          <h1 className='text-2xl font-bold'> Our Mission</h1>
+          <p>At MoveZone, we believe that everyone deserves easy access to the magic of movies. Our mission is to revolutionize the way you experience cinema by providing a seamless, intuitive, and delightful ticket booking experience.</p>
+          <p>Whether you're planning a date night, a family outing, or a solo adventure into the world of film, MoveZone is here to make your experience unforgettable from the moment you book to the final credits.</p>
+          <Button title={"Start Booking "} variant='primary' size='md' onClick={()=>navigate("/movie")}/>
+        </div>
+        <div>
+          <img src="" alt="" />
+        </div>
       </div>
     </div>
   )
