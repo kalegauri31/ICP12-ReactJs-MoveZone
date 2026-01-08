67d33c7832dc29650de2bc2217ae8694f826262a
siddhikharde <siddhikharde2005@gmail.com>
2026-01-08 19:33:10 +0530

    fav movies fatched from local storage and desply there lenght on ui

--- src/views/FavoriteMovies.jsx
+++ src/views/FavoriteMovies.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
-
+import { useState, useEffect } from 'react'
 function FavoriteMovies() {
+    const [favMovies, setFavMovies]=useState([]);
+
+    useEffect(()=>{
+       const storedMovies=JSON.parse(localStorage.getItem("favoriateMovies") || "[]");
+        setFavMovies(storedMovies);     
+    },[])
   return (
-    <div>
-      
+    <div className='bg-[#1B3C53] min-h-screen'>
+        <h1>{favMovies.length}</h1>
     </div>
   )
 }
